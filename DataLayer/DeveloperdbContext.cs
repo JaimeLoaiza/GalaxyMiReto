83b4bc8504cb22e732772512ee8d61b49930d05f
JaimeLoaiza <jaimeloaiza@hotmail.es>
2020-08-02 22:06:00 -0500

    Migraciones Code First

---
+++ DataLayer/DeveloperdbContext.cs
@@ -0,0 +1,19 @@
+﻿using GalaxyMiReto.GalaxyMiReto.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GalaxyMiReto.DataLayer
+{
+    public class DeveloperdbContext:DbContext
+    {
+        public DeveloperdbContext(DbContextOptions
+            <DeveloperdbContext>options):base(options)
+        { 
+        }
+        public DbSet<Developer>Developer { get; set; }
+        public DbSet<Contrato> Contrato { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,12 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GalaxyMiReto.DataLayer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace GalaxyMiReto
 {
@@ -24,6 +28,9 @@ namespace GalaxyMiReto
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<DeveloperdbContext>
+                (o => o.UseSqlServer(Configuration.GetConnectionString("DeveloperDatabase")));
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,12 +40,7 @@ namespace GalaxyMiReto
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
