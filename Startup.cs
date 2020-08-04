3b1038316a60a01cb0068e8ec6aa6048ae3e25ae
JaimeLoaiza <jaimeloaiza@hotmail.es>
2020-08-03 22:27:09 -0500

    Migraciones Code First

--- Startup.cs
+++ Startup.cs
@@ -34,13 +34,13 @@ namespace GalaxyMiReto
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, DeveloperdbContext db)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            
+            db.Database.EnsureCreated();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
